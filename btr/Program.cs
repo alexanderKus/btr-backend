8f37856adf316321119fd38ff93f84a8a0905c85
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-03-06 22:33:57 +0100

    Register as singleton

--- btr/Program.cs
+++ btr/Program.cs
@@ -4,7 +4,7 @@ using btr.Servies.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddTransient<IBookService, BookService>();
+builder.Services.AddSingleton<IBookService, BookService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
